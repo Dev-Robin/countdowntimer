6823deff4605cbc9fa219f8eac253dcf30f47f3a
Dev-Robin <58303612+Dev-Robin@users.noreply.github.com>
2024-09-09 17:16:27 +0200

    started building timer

---
+++ src/components/CountDownTimer.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { ref, type Ref } from 'vue';
+
+    const time : Ref<String> = ref("hi");
+    time.value = "world";
+</script>
+<template>
+    <h1>{{ time }}</h1>
+</template>
